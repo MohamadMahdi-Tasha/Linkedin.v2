ba6a96d3555a31b2090d8b3007e50bc2279381fa
MohamadMahdi-Tasha <imwhdiiii@gmail.com>
2022-01-25 13:54:08 +0330

    creted styles for header component

--- src/Pages/feed/components/header.js
+++ src/Pages/feed/components/header.js
@@ -8,63 +8,57 @@ import Profile_Picture from "../../../components/profile-picture";
 class Header extends Component {
     render() {
         return (
-            <header>
-                <div className="left-side">
-                    <i className="bi bi-linkedin"></i>
-                    <div className={'search-div'}>
-                        <form action="#">
-                            <button type={'submit'}><i className="bi bi-search"></i></button>
-                            <input type="text" placeholder={'Search'}/>
+            <header className={'p-fixed bgc-white width-100 dis-inline-flex'}>
+                <div className="left-side width-50 dis-inline-flex ai-center mr-1">
+                    <i className="bi bi-linkedin fs-3 mr-1 c-blue ml-1"></i>
+                    <div className={'search-div bg-skyblue width-100 pdot5 roundeddot5 dis-inline-flex ai-center input-hover-focus'}>
+                        <form action="#" className={'width-100 dis-inline-flex ai-center'}>
+                            <button className={'search-btn pdot5 bg-none border-none c-blue '} type={'submit'}><i className="bi bi-search"></i></button>
+                            <input className={'search-input width-100 border-none bg-none  c-blue placeholder-blue outline-none'} type="text" placeholder={'Search'}/>
                         </form>
                     </div>
                 </div>
-                <div className="right-side">
+                <div className="right-side width-50 dis-inline-flex jc-sb">
                     <a href="#">
-                        <button>
+                        <button className={'dis-flex flex-d-c jc-center ai-center pdot5 bg-none border-none bg-none-btn-hover h-100 header-btn-active'}>
                             <i className="bi bi-house-fill"></i>
                             Home
                         </button>
                     </a>
                     <a href="#">
-                        <button>
+                        <button className={'dis-flex flex-d-c jc-center ai-center pdot5 bg-none border-none bg-none-btn-hover h-100 border-bottom-c-none'}>
                             <i className="bi bi-people-fill"></i>
                             My Network
                         </button>
                     </a>
                     <a href="#">
-                        <button>
+                        <button className={'dis-flex flex-d-c jc-center ai-center pdot5 bg-none border-none bg-none-btn-hover h-100 border-bottom-c-none'}>
                             <i className="bi bi-bag-fill"></i>
                             Jobs
                         </button>
                     </a>
                     <a href="#">
-                        <button>
+                        <button className={'dis-flex flex-d-c jc-center ai-center pdot5 bg-none border-none bg-none-btn-hover h-100 border-bottom-c-none'}>
                             <i className="bi bi-chat-right-text-fill"></i>
                             Massaging
                         </button>
                     </a>
                     <a href="#">
-                        <button>
+                        <button className={'dis-flex flex-d-c jc-center ai-center pdot5 bg-none border-none bg-none-btn-hover h-100 border-bottom-c-none'}>
                             <i className="bi bi-bell-fill"></i>
                             Notifications
                         </button>
                     </a>
                     <a href="#">
-                        <button>
-                            <Profile_Picture first_name={login_name.slice(0,1).toUpperCase()}/>
-                            <div>
-                                Me
-                                <i className="bi bi-chevron-compact-down"></i>
-                            </div>
+                        <button className={'dis-flex flex-d-c jc-center ai-center pdot5 bg-none border-none bg-none-btn-hover h-100 border-bottom-c-none'}>
+                            <i className="bi bi-person-fill"></i>
+                            Me
                         </button>
                     </a>
                     <a href="#">
-                        <button>
+                        <button className={'dis-flex flex-d-c jc-center ai-center pdot5 bg-none border-none bg-none-btn-hover h-100  '}>
                             <i className="bi bi-grid-3x3-gap-fill"></i>
-                            <div>
-                                Works
-                                <i className="bi bi-chevron-compact-down"></i>
-                            </div>
+                            Works
                         </button>
                     </a>
                 </div>
