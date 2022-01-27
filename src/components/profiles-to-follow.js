be49f11a267a921f78e4b5891d63a37e855ae8f9
MohamadMahdi-Tasha <imwhdiiii@gmail.com>
2022-01-27 08:52:19 +0330

    creted styles of profile to follow component

--- src/components/profiles-to-follow.js
+++ src/components/profiles-to-follow.js
@@ -3,13 +3,15 @@
 function ProfilesToFollow(props) {
     return (
         <a href="#">
-            <div>
-                <div className={'width-100 p2 bg-size-cover'} style={{backgroundImage: `url(${props.image})`}}></div>
-                <div>
-                    <h3>{props.name}</h3>
-                    <h5>{props.followersNumber} Followers</h5>
+            <div className={'dis-inline-flex width-100 jc-sb ai-center'}>
+                <div className={'dis-inline-flex ai-center'}>
+                    <div className={'w-4rem h-4rem bg-size-cover mr-1'} style={{backgroundImage: `url(${props.image})`}}></div>
+                    <div>
+                        <h3 className={'c-black fw'}>{props.name}</h3>
+                        <h5 className={'c-light-gray'}>{props.followersNumber} Followers</h5>
+                    </div>
                 </div>
-                <button>
+                <button className={'c-blue bg-none fs-1 dis-inline-flex border-none p1 btn-bgnone-hover roundeddot5'}>
                     <i className="bi bi-plus"></i>
                     Follow
                 </button>
